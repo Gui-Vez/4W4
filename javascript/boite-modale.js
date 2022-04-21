a8f0d4978c7acf98ae545c3b35fb9a1d7c106091
Guillaume Vézina <vezina.guillaume@outlook.com>
2022-04-21 16:35:49 -0400

    s12 c1 GV - Étape 2 (Suite)

--- javascript/boite-modale.js
+++ javascript/boite-modale.js
@@ -1,17 +1,34 @@
 (function()
 {
+    // Variable boite modale
     let boite__modale = document.querySelector(".boite__modale");
     
+    // Variable description du cours
     let cours__desc__ouvrir = document.querySelectorAll('.cours__desc__ouvrir');
 
+    let boite__modale__fermeture = document.querySelector('.boite__modale__fermeture');
 
 
+    // Pour chaque bouton des descriptions de cours,
     for (const bout of cours__desc__ouvrir)
     {
+        // Si l'on clique sur le bouton,
         bout.addEventListener('mousedown', function()
         {
+            // S'il n'y a pas de classe, ajouter la classe "ouvrir"
+            if (!boite__modale.classList.contains('ouvrir'))
             boite__modale.classList.add('ouvrir');
-            console.log(boite__modale.classList)
+
+            // Sinon, l'enlever
+            else
+            boite__modale.classList.remove('ouvrir');
         })
     }
+
+    // Si l'on clique sur le bouton de fermeture de la boite modale,
+    boite__modale__fermeture.addEventListener('mousedown', function()
+    {
+        // Enlever la classe "ouvrir"
+        boite__modale.classList.remove('ouvrir');
+    })
 })()
